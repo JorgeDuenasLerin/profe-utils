702e8813e74db4f4410897f746004e9979639d9f
Jorge Duenas Lerín <jorge.duenas.lerin@gmail.com>
2020-11-30 15:26:00 +0100

    Probando con Chormium

--- automa-web/router_ejemplo2.py
+++ automa-web/router_ejemplo2.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+DRIVER='./chromedriver'
+URL='http://192.168.10.1/login.html'
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
+
+chromeOptions = Options()
+chromeOptions.headless = True
+
+
 #This example requires Selenium WebDriver 3.13 or newer
-with webdriver.Firefox() as driver:
+with webdriver.Chrome(executable_path=DRIVER, options=chromeOptions) as driver:
     wait = WebDriverWait(driver, 10)
-    driver.get("http://192.168.0.1/login.html")
-    driver.find_element(By.CSS, 'input[type="text"]').send_keys("sercomm")
-    driver.find_element(By.CSS, 'input[type="password"]').send_keys("1234")
+    driver.get(URL)
+    driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys("sercomm")
+    driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys("1234")
     #Usuario y contraseña incorrectos
-    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "div#message>div#message-error>h2")))
+    driver.find_element(By.CSS_SELECTOR, 'input[type="button"].button-apply-wide').click()
+    print('Peticion:' + driver.page_source)
+    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "div#message-error-text")))
     print(first_result.text)
