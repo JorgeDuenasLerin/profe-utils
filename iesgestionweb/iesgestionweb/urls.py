b0865be708cc38d5b51a70e3674a9dabc6a1d0af
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-05-07 11:30:42 +0200

    En el alta se vuelve al listado concreto

--- iesgestionweb/iesgestionweb/urls.py
+++ iesgestionweb/iesgestionweb/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', include('incidencias.urls')),
+    re_path(r'^$', RedirectView.as_view(url='incidencias/', permanent=False), name='index'),
+    path('incidencias/', include('incidencias.urls')),
     path('admin/', admin.site.urls),
 ]
--- iesgestionweb/incidencias/views.py
+++ iesgestionweb/incidencias/views.py
@@ -10,10 +10,12 @@ from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
 from django.views.generic.edit import FormView
 
+from django.urls import reverse
+
 class IncidenciaListView(SingleTableMixin, FilterView):
 
     table_class = IncidenciaTable
-    queryset = Incidencia.objects.all()
+    queryset = Incidencia.objects.all().order_by('-created_at')
     template_name = 'incidencias/index.html'
     filterset_class = IncidenciaFilter
 
@@ -35,12 +37,16 @@ class IncidenciaListView(SingleTableMixin, FilterView):
 class IncidenciaFormView(FormView):
     template_name = 'incidencias/incidencia.html'
     form_class = IncidenciaForm
-    success_url = '/'
+
+    def get_success_url(self):
+        return reverse('incidencias:listado')+"?lugar="+str(self.return_lugar.id)
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         #form.send_email()
         form.save()
+
+        self.return_lugar = form.cleaned_data.get('lugar')
 
         return super().form_valid(form)
