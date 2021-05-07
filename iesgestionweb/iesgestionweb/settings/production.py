205329b9d1b9c3e00eff3e29dc0d4081a878c25e
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-05-07 12:41:22 +0200

    Preparando el despliegue

---
+++ iesgestionweb/iesgestionweb/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+import json
+
+with open(BASE_DIR / 'iesgestionweb/settings/secrets.json') as f:
+    config = json.load(f)
+
+print(config)
+
+ALLOWED_HOSTS = ['localhost']
+DEBUG = False
+DATABASES = config["databases"]
