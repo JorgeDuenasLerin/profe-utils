cdbdb917c6e43a0a3cb05a7a055ba2e4c6ee4073
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-05-10 13:52:48 +0200

    Avanzando en deploy

--- iesgestionweb/bin/deploy.sh
+++ iesgestionweb/bin/deploy.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No secret.json"
+  exit
+fi
+
+su -
+
 git pull
 
-cp manage.py /var/www/iesgestionweb/
-cp -r iesgestionweb/ /var/www/iesgestionweb/
-cp -r incidencias/ /var/www/iesgestionweb/
+
+DST_PATH="/var/www/iesgestionweb/"
+
+
+
+echo "Copying files to /var/"
+
+cp manage.py $DST_PATH
+cp -r iesgestionweb/ $DST_PATH
+cp -r incidencias/ $DST_PATH
