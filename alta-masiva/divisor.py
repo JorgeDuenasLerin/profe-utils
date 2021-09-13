b9342fe9f47389c2049dad7d68edd169bc605e6f
Jorge Duenas Lerín <jorge.duenas.lerin@gmail.com>
2021-09-13 13:14:15 +0200

    Padding con ceros

--- alta-masiva/divisor.py
+++ alta-masiva/divisor.py
@@ -17,15 +17,18 @@ listado['apellido']=listado['Primer apellido'] + ' ' + listado['Segundo apellido
 listado['apellido']=listado['apellido'].str.strip()
 listado['apellido_ascii']=listado['apellido'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 listado['Unidad']=listado['Unidad'].fillna('SIN_UNIDAD')
+listado['id']=listado['Nº id. Alumno'].astype(str).str.pad(8,fillchar='0')
 
 listado = listado.sort_values(by=['Unidad','apellido_ascii','Nombre'])
-unidades = listado.Unidad.unique()
+unidades = sorted(listado.Unidad.unique())
 
+print("Listado de unidades")
 for unidad in unidades:
+    print(unidad)
     filtrado = listado[listado.Unidad == unidad]
     filtrado.to_csv(
                 path_or_buf='alta_'+unidad+'.csv',
-                columns=['centro','Nombre', 'apellido','Nº id. Alumno'],
+                columns=['centro','Nombre', 'apellido','id'],
                 index=False,
                 header=False,
                 encoding='utf-8'
