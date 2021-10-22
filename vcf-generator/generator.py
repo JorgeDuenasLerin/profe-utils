f3aa6a3284533a8876193b9e362c779d2c37d136
jorge dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-10-22 13:54:18 +0200

    Creado de VCF

---
+++ vcf-generator/generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+VCARD_LINE="BEGIN:VCARD"\
+            "VERSION:3.0"\
+            "FN:%s"\
+            "EMAIL;TYPE=INTERNET;TYPE=OTHER:%s"\
+            "CATEGORIES:%s"\
+            "END:VCARD"\
