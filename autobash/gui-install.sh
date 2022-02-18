e40fe89b458953a22e80391036a9ace624369dc7
Jorge Duenas Lerín <jorge.duenas.lerin@gmail.com>
2022-02-18 15:07:23 +0100

    Intentando subida e instalación remota

--- autobash/gui-install.sh
+++ autobash/gui-install.sh
@@ -1,1 +1,12 @@
-export DISPLAY=:0; konsole --noclose -e 'ls'
+#!/usr/bin/env bash
+
+IP=$(cat $1)
+PASS="${AUTOBASHPASS}"
+CMD=$2
+
+# Ejem: "apt-get -y install php7.4-cli"
+
+for con in $IP; do
+  echo $con
+  echo $PASS | sshpass -p"$PASS" -v ssh -o "StrictHostKeyChecking no" -t $con "sudo -S \$(export DISPLAY=:0;konsole --noclose -e '$CMD')"
+done;
